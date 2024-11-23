bdf28310fc588d6d7683e4fe4316edfa05cd049d
Zaveer Khan <zaveerkhan.fire@gmail.com>
2024-11-23 21:13:08 +0500

    created the ui for the hero section.

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,12 +1,25 @@
 export const Header = () => {
-  return <header>
-    <div className="flex items-center justify-center fixed top-3 w-full">
-      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-      <a href="#" className="nav-item">Home</a>
-      <a href="#" className="nav-item">Projects</a>
-      <a href="#" className="nav-item">About</a>
-      <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
-      </nav>
-    </div>
-  </header>;
+  return (
+    <header>
+      <div className='flex items-center justify-center fixed z-10 top-3 w-full'>
+        <nav className='flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur'>
+          <a href='#' className='nav-item'>
+            Home
+          </a>
+          <a href='#' className='nav-item'>
+            Projects
+          </a>
+          <a href='#' className='nav-item'>
+            About
+          </a>
+          <a
+            href='#'
+            className='nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900'
+          >
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 };
