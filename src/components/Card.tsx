dd920ad4d960e3f0f647abc9c367e3c6d7c65248
Zaveer Khan <zaveerkhan.fire@gmail.com>
2024-11-28 20:26:04 +0500

    created a few reusable components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export const Card = ({
   return (
     <div
       className={twMerge(
-        "bg-gray-800 rounded-3xl z-0 after:z-10 overflow-hidden relative after:absolute after:content-[''] after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none p-6",
+        "bg-gray-800 rounded-3xl z-0 after:z-10 overflow-hidden relative after:absolute after:content-[''] after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
         className
       )}
     >
