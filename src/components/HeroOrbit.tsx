9bc57d65208f997766fe0095bdebf8021e2cc2d5
Zaveer Khan <zaveerkhan.fire@gmail.com>
2024-12-02 04:51:05 +0500

    added animations in hero section

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,27 +1,51 @@
 import React, { PropsWithChildren } from 'react';
-import StarIcon from '@/assets/icons/star.svg';
+import { twMerge } from 'tailwind-merge';
 
 const HeroOrbit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  shouldOrbit = false,
+  orbitDuration,
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  shouldOrbit?: boolean;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+  orbitDuration?: string;
+}>) => {
   return (
-    <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+    <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20'>
       <div
-        className='flex items-start justify-start'
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          height: `${size}px`,
-          width: `${size}px`,
-        }}
+        className={twMerge(shouldOrbit === true && 'animate-spin')}
+        style={{ animationDuration: orbitDuration }}
       >
-      <div className='inline-flex' style={{
-        transform: `rotate(${rotation * -1}deg)`
-      }}>
-        {children}
+        <div
+          className='flex items-start justify-start'
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            height: `${size}px`,
+            width: `${size}px`,
+          }}
+        >
+          <div
+            className={twMerge(shouldSpin === true && 'animate-spin')}
+            style={{ animationDuration: spinDuration }}
+          >
+            <div
+              className='inline-flex'
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
