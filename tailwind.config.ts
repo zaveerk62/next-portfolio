264bb93e54c8735fca06a706819256669c1e289a
Zaveer Khan <zaveerkhan.fire@gmail.com>
2024-12-02 05:17:06 +0500

    added the marquee animation to the tape

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     screens: {
-      sm: "375px",
-      md: "768px",
-      lg: "1200px",
+      sm: '375px',
+      md: '768px',
+      lg: '1200px',
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: "1rem",
-        md: "2rem",
+        DEFAULT: '1rem',
+        md: '2rem',
       },
     },
     extend: {
@@ -27,16 +27,24 @@ const config: Config = {
       },
       animation: {
         'ping-large': 'ping-large 1s ease-in-out infinite',
+        'move-left': 'move-left 1s linear infinite',
       },
       keyframes: {
         'ping-large': {
           '75%, 100%': {
             transform: 'scale(3)',
             opacity: '0',
-
           }
-        }
-      }
+        },
+        'move-left': {
+          '0%': {
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            transform: 'translateX(-50%)',
+          }
+        },
+      },
     },
   },
   plugins: [],
